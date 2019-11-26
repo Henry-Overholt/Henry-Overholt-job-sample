20d9b70195a79238cad67be2edc9e388c698004c
Henry <henrysoverholt@gmail.com>
2019-11-25 22:30:51 -0500

    store counts in cookies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { VideoPlayerComponent } from './video-player/video-player.component';
+import { AppComponent } from "./app.component";
+import { VideoPlayerComponent } from "./video-player/video-player.component";
+import { CookieService } from "ngx-cookie-service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    VideoPlayerComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, VideoPlayerComponent],
+  imports: [BrowserModule],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { url } from "inspector";
+import { CookieService } from "ngx-cookie-service";
 
 @Component({
   selector: "app-video-player",
@@ -7,18 +7,6 @@ import { url } from "inspector";
   styleUrls: ["./video-player.component.css"]
 })
 export class VideoPlayerComponent implements OnInit {
-  videoPlaying: any = {
-    picture:
-      "https://static-email-hosting.s3.amazonaws.com/24G_Test_Project/videos/ces_overview.jpg",
-    video:
-      "https://static-email-hosting.s3.amazonaws.com/24G_Test_Project/videos/ces_overview.mp4",
-    name: "CES Overview",
-    views: 0,
-    likes: 0,
-    dislikes: 0,
-    index: 1,
-    selected: false
-  };
   videoLib: any[] = [
     {
       picture:
@@ -26,11 +14,12 @@ export class VideoPlayerComponent implements OnInit {
       video:
         "https://static-email-hosting.s3.amazonaws.com/24G_Test_Project/videos/who_is_24g.mp4",
       name: "Who is 24G?",
-      views: 0,
-      likes: 0,
-      dislikes: 0,
+      views: this.cookieService.get("firstViews"),
+      likes: this.cookieService.get("firstLikes"),
+      dislikes: this.cookieService.get("firstDislikes"),
+      code: "first",
       index: 0,
-      selected: false
+      selected: true
     },
     {
       picture:
@@ -38,11 +27,12 @@ export class VideoPlayerComponent implements OnInit {
       video:
         "https://static-email-hosting.s3.amazonaws.com/24G_Test_Project/videos/ces_overview.mp4",
       name: "CES Overview",
-      views: 0,
-      likes: 0,
-      dislikes: 0,
+      views: this.cookieService.get("secondViews"),
+      likes: this.cookieService.get("secondLikes"),
+      dislikes: this.cookieService.get("secondDislikes"),
+      code: "second",
       index: 1,
-      selected: true
+      selected: false
     },
     {
       picture:
@@ -50,17 +40,36 @@ export class VideoPlayerComponent implements OnInit {
       video:
         "https://static-email-hosting.s3.amazonaws.com/24G_Test_Project/videos/future_of_drones.mp4",
       name: "Future of Drones",
-      views: 0,
-      likes: 0,
-      dislikes: 0,
+      views: this.cookieService.get("thirdViews"),
+      likes: this.cookieService.get("thirdLikes"),
+      dislikes: this.cookieService.get("thirdDislikes"),
+      code: "third",
       index: 2,
       selected: false
     }
   ];
+  videoPlaying: any = this.videoLib[0];
 
-  constructor() {}
+  constructor(private cookieService: CookieService) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.startCookies();
+    console.log(this.cookieService.getAll());
+  }
+  startCookies() {
+    if (this.cookieService.get("start") === "0") {
+      this.cookieService.set("start", "1");
+      this.cookieService.set("firstDislikes", "0");
+      this.cookieService.set("firstLikes", "0");
+      this.cookieService.set("firstViews", "0");
+      this.cookieService.set("secondDislikes", "0");
+      this.cookieService.set("secondLikes", "0");
+      this.cookieService.set("secondViews", "0");
+      this.cookieService.set("thirdDislikes", "0");
+      this.cookieService.set("thirdLikes", "0");
+      this.cookieService.set("thirdViews", "0");
+    }
+  }
   handleVidClick(i: number) {
     this.videoLib.forEach(video => {
       video.selected = false;
@@ -69,12 +78,30 @@ export class VideoPlayerComponent implements OnInit {
     this.videoPlaying = this.videoLib[i];
   }
   handleLike(i: number) {
+    let oldLikes = parseInt(
+      this.cookieService.get(`${this.videoLib[i].code}Likes`)
+    );
+    oldLikes++;
+    let newLikes = oldLikes.toString();
+    this.cookieService.set(`${this.videoLib[i].code}Likes`, newLikes);
     this.videoLib[i].likes++;
   }
   handleDislike(i: number) {
+    let oldDislikes = parseInt(
+      this.cookieService.get(`${this.videoLib[i].code}Dislikes`)
+    );
+    oldDislikes++;
+    let newDislikes = oldDislikes.toString();
+    this.cookieService.set(`${this.videoLib[i].code}Dislikes`, newDislikes);
     this.videoLib[i].dislikes++;
   }
   handleViews(i: number) {
+    let oldViews = parseInt(
+      this.cookieService.get(`${this.videoLib[i].code}Views`)
+    );
+    oldViews++;
+    let newViews = oldViews.toString();
+    this.cookieService.set(`${this.videoLib[i].code}Views`, newViews);
     this.videoLib[i].views++;
   }
 }
